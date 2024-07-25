1580930e305a6d87d1c43a57e244108429e47b47
Surajss2290 <surajsuryawanshi2290@gmail.com>
2024-07-25 19:38:38 +0530

    Second update 24-07-2024

--- src/MethodeRef/stuff.java
+++ src/MethodeRef/stuff.java
@@ -3,5 +3,7 @@ package MethodeRef;
 public class stuff {
     public  static void     dostuff(){
         System.out.println("I am doing task");
+
+        System.out.println("I am not doing task");
     }
 }
